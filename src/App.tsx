577eb9208a42d40c92a195818dde7bd7349f7978
Akash Chaudhari <138999370+akashch1512@users.noreply.github.com>
2024-07-16 20:08:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,25 @@ import Coins from './icons/Coins';
 const App: React.FC = () => {
   
   const handleMineIconClick = () => {
-    // useNavigate("https://akashchaudhari1512.tiiny.site/");
-    window.location.href = "https://page2game.netlify.app/"
+    window.location.href = "https://page2game.netlify.app/"; // 
   };
-  
+
   const handleExchangeIconClick = () => {
-    console.log("exchnage icon clicked"); // Replace with your actual path
+     window.location.href = "https://falconhamster.netlify.app/"; // 
   };
 
   const handleFriendsIconClick = () => {
-    console.log("freinds icon click") // Replace with your actual path
+    window.location.href = "https://page3game.netlify.app/"; //
   };
 
   const handleEarnIconClick = () => {
-
-    window.location.href = "https://page3game.netlify.app/"
+    window.location.href = "https://exchnagepageakash.netlify.app/"; // 
   };
 
   const handlehamsterCoinIconClick = () => {
-    window.location.href = 'https://page1game.netlify.app/'; 
-
-  }
+    window.location.href = "https://page1game.netlify.app/"; //  airdrop
+  };
+  
   return (
     <div onClick={handleExchangeIconClick} className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
         <div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl">
